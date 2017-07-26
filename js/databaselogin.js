09b891ab768010fde880491706e97bb62a26684e
TaterHater <nick_beeson@yahoo.com>
2017-07-26 12:44:10 -0600

    fixed auth shit

--- js/databaselogin.js
+++ js/databaselogin.js
@@ -10,3 +10,12 @@ con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
+
+function sendMessage(message) {
+    con.query("INSERT INTO messages (message, username,timestamp) VALUES('" + message + "', 'username', 'time'))");
+}
+
+postButton.addEventListener("click", function () {
+
+
+});
