f71d3ed1c4f29b53f3d8904d9d77f3fa781e411e
huntinator7 <huntinator7@gmail.com>
2017-08-01 15:49:37 -0600

    Added index call to index2

--- chat/index2.js
+++ chat/index2.js
@@ -3,9 +3,11 @@ var app = express();
 
 var chat = require('./chat.js');
 var login = require('./login.js');
+var index = require('./index.js');
 
 //both index.js and things.js should be in same directory
 app.use('/chat', chat);
 app.use('/login', login);
+app.use('/index', index);
 
 app.listen(80);
