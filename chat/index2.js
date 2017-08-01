0a1737be45712e4e313d84913fa27da2fb5e0d52
huntinator7 <huntinator7@gmail.com>
2017-08-01 15:24:15 -0600

    Changed to port 80, multi file test

--- chat/index2.js
+++ chat/index2.js
@@ -5,5 +5,6 @@ var things = require('./things.js');
 
 //both index.js and things.js should be in same directory
 app.use('/things', things);
+app.use('/login', login);
 
-app.listen(3000);
+app.listen(80);
