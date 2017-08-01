40ed72e8457cd48d7dc22affd3a547df992fa964
huntinator7 <huntinator7@gmail.com>
2017-08-01 03:43:32 -0600

    Trying to fix font issue - CORS

--- chat/index.js
+++ chat/index.js
@@ -10,7 +10,11 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 app.use(siofu.router);
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/indexchat.html');
