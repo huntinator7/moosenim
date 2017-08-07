b58ebc226efa354d0fb8db2b86452d22c6a39319
TaterHater <nick_beeson@yahoo.com>
2017-08-07 08:51:18 -0600

    added console log

--- chat/chat.js
+++ chat/chat.js
@@ -4,16 +4,21 @@ var path = require('path');
 app = express();
 
 
-router.get('/', function(req, res){
+
+app.get('/', function (req, res) {
+    console.log("loading chat.html");
+    res.sendFile(path.join(__dirname + '/chat.html'));
+});
+
+router.get('/', function (req, res) {
+    console.log("loading chat.html");
     res.sendFile(path.join(__dirname + '/chat.html'));
 });
 router.post('/', function(req, res){
    res.send('POST route on chat.');
 });
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/chat.html'));
-});
+
 
 //export this router to use in our index.js
 module.exports = router;
