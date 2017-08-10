5d3f4c0191787dfe9e349906cb363a127ed4039e
TaterHater <nick_beeson@yahoo.com>
2017-08-09 22:45:08 -0600

    login log

--- chat/index2.js
+++ chat/index2.js
@@ -15,8 +15,8 @@ app.use('/', login);
 
 io.sockets.on('connection', function (socket) {
     console.log('A user connected - index2.js');
-    socket.on('login message', function (name) {
-        console.log(name);
+    socket.on('login message', function (displayName, email, photoURL, uid) {
+        console.log(name+" " + email);
     });
     socket.on('ping', function (name) {
         console.log('pong');
