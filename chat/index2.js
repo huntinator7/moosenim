69b766368a5df3bf943a07ced1c116da9cd6eb98
TaterHater <nick_beeson@yahoo.com>
2017-08-09 19:40:15 -0600

    comented out any mention of socktest.

--- chat/index2.js
+++ chat/index2.js
@@ -8,13 +8,13 @@ var siofu = require("socketio-file-upload");
 var chat = require('./chat.js');
 var login = require('./login.js');
 // var index = require('./index.js');
-var socktest = require('./socktest.js');
+//var socktest = require('./socktest.js');
 
 //both index.js and things.js should be in same directory
 app.use('/chat', chat);
 app.use('/login', login);
 // app.use('/index', index);
-app.use('/socktest', socktest);
+//app.use('/socktest', socktest);
 
 io.sockets.on('connection', function (socket) {
     console.log('client connect');
