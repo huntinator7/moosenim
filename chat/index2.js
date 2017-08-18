523dead7868d1f8f1f79e3b156252162d28e1800
TaterHater <nick_beeson@yahoo.com>
2017-08-18 16:47:11 -0600

    added another fake room

--- chat/index2.js
+++ chat/index2.js
@@ -262,7 +262,7 @@ function getrooms(uid) {
     list.push(1);
     list.push(2);
     list.push(3);
-
+    list.push(4);
     con.query("SELECT * FROM room_users WHERE users_id = ?", [uid], function (error, rows) {
        // for (var i = 0; i < rows.length - 1; i++) {
          //   list.push(rows[i]);
