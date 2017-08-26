fa962f7a19aaf5a51df8528b20546269358a9ef9
huntinator7 <huntinator7@gmail.com>
2017-08-26 17:18:11 -0600

    Attempt to fix MySQL disconnect when server's internet shits itself

--- chat/index2.js
+++ chat/index2.js
@@ -35,13 +35,6 @@ app.use('/', login);
 app.use("/images", express.static(__dirname + '/images'));
 app.use("/uploads", express.static(__dirname + '/uploads'));
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "raspberry",
-    database: "moosenim"
-});
-
 //Main socket.io listener
 io.sockets.on('connection', function (socket) {
 
@@ -177,6 +170,38 @@ io.sockets.on('connection', function (socket) {
     });
 
 });
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "raspberry",
+    database: "moosenim"
+});
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(con); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 //connects to mysql database
 con.connect(function (err) {
