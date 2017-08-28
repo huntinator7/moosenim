342aa43423f708d9b62762acd55a0cd699e5c533
huntinator7 <huntinator7@gmail.com>
2017-08-27 22:31:22 -0600

    discord url for image 6 - image should show

--- chat/index2.js
+++ chat/index2.js
@@ -24,11 +24,14 @@ client.on('ready', () => {
 //Any time a Discord message is sent, bot checks to see if in moosen-im channel and if not sent by bot. If so, it adds the message to the DB and emits it
 client.on('message', msg => {
     if (msg.channel.id == 329020807487553537 && !(msg.author.bot)) {
-        if (msg.attachments) {
-            console.log(msg.attachments.first().url);
-        }
         sendMessage(msg.content, msg.author.username, 1, 1);
         getMessageDiscord(msg.author.username, msg.content, msg.author.avatarURL);
+        if (msg.attachments) {
+            console.log(msg.attachments.first().url);
+            var message = '<img class="materialboxed responsive-img" src="' + msg.attachments.first().url + '" alt="Error - Image not found">';
+            sendMessage(message, msg.author.username, 1, 1);
+            getMessageDiscord(msg.author.username, message, msg.author.avatarURL);
+        }
         console.log(msg.author.username + ': ' + msg.content);
     }
 });
