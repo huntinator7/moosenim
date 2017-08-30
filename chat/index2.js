1b1d39458bd04451486ccd500d46757ea1dab79d
huntinator7 <huntinator7@gmail.com>
2017-08-30 11:30:56 -0600

    Server borked, sending to dev test instead

--- chat/index2.js
+++ chat/index2.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
 
 //Any time a Discord message is sent, bot checks to see if in moosen-im channel and if not sent by bot. If so, it adds the message to the DB and emits it
 client.on('message', msg => {
-    if (msg.channel.id == 329020807487553537 && !(msg.author.bot)) {
+    if (msg.channel.id == 319938734135050240 && !(msg.author.bot)) {
         msg.channel.members.forEach(function (element){
             try {
                 console.log(`Username: ${element.displayName}`);
@@ -47,7 +47,8 @@ client.on('message', msg => {
         console.log(msg.author.username + ': ' + msg.content);
     }
 });
-//329020807487553537
+//329020807487553537 - moosen-im
+//319938734135050240 - dev-test
 
 //Associating .js files with URLs
 app.use('/chat/main', chat);
