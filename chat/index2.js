32052369e3dc853cd29e986c0d411dfbfee17db4
huntinator7 <huntinator7@gmail.com>
2017-08-30 01:00:24 -0600

    Fixing room = 1 error 5

--- chat/index2.js
+++ chat/index2.js
@@ -175,6 +175,7 @@ io.sockets.on('connection', function (socket) {
                     sendMessage(newmsg, un, uid, curroom);
                 }
             } else {
+                console.log('In chat message, chatid: ' + chatid + ' roomid: ' + roomid);
                 sendMessage(msg, un, uid, curroom);
             }
             io.emit(getMessage(curroom));
