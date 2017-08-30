ff8d274d8fa818f63de97fde76c2c9d88f010bfe
huntinator7 <huntinator7@gmail.com>
2017-08-30 00:37:37 -0600

    Log for socket.rooms 2

--- chat/index2.js
+++ chat/index2.js
@@ -105,7 +105,7 @@ io.sockets.on('connection', function (socket) {
     });
     //Generic message emit
     socket.on('chat message', function (msg) {
-        console.log(Objects.keys(socket.rooms));
+        console.log('socket.rooms: ' + Objects.keys(socket.rooms) + ' END');
         var un = 'Error - Username Not Found';
         var uid;
         var curroom;
