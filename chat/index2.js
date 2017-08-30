a36dc97a59165a056eaf8676d9e4063bfeff87b6
huntinator7 <huntinator7@gmail.com>
2017-08-30 15:26:36 -0600

    Removed image change

--- chat/index2.js
+++ chat/index2.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
 
 //Any time a Discord message is sent, bot checks to see if in moosen-im channel and if not sent by bot. If so, it adds the message to the DB and emits it
 client.on('message', msg => {
-    client.user.setAvatar('./images/discord.png');
+    // client.user.setAvatar('./images/discord.png');
     if (msg.channel.id == 329020807487553537 && !(msg.author.bot)) {
         msg.channel.members.forEach(function (element){
             try {
