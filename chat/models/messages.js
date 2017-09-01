a6be7b7bfc0d2767af2c8d512fc26199a77a5dc4
TaterHater <nick_beeson@yahoo.com>
2017-09-01 12:02:05 -0600

    api test typo

--- chat/models/messages.js
+++ chat/models/messages.js
@@ -5,7 +5,7 @@ var db = require('../RESTmessages');
 var messages = {
 
     GetLastMessages: function (id,callback) {
-        return db.query("SELECT * FROM ( SELECT * FROM messages WHERE chatroom_id = ? ORDER BY id DESC LIMIT 10) sub ORDER BY  id ASC", [id, num]);
+        return db.query("SELECT * FROM ( SELECT * FROM messages WHERE chatroom_id = ? ORDER BY id DESC LIMIT 10) sub ORDER BY  id ASC", [id]);
 
     },
 
