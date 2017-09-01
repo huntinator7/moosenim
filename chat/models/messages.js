070a80c933e94f34cbd60d881f7a8816a2d68788
TaterHater <nick_beeson@yahoo.com>
2017-09-01 12:03:49 -0600

    apitest2

--- chat/models/messages.js
+++ chat/models/messages.js
@@ -1,11 +1,18 @@
 
-
+var sql = require('mysql');
+var connect = {
+    host: "localhost",
+    user: "root",
+    password: "raspberry",
+    database: "moosenim"
+};
+module.exports = connect;
 var db = require('../RESTmessages');
 
 var messages = {
 
     GetLastMessages: function (id,callback) {
-        return db.query("SELECT * FROM ( SELECT * FROM messages WHERE chatroom_id = ? ORDER BY id DESC LIMIT 10) sub ORDER BY  id ASC", [id]);
+        return connect.query("SELECT * FROM ( SELECT * FROM messages WHERE chatroom_id = ? ORDER BY id DESC LIMIT 10) sub ORDER BY  id ASC", [id]);
 
     },
 
