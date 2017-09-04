83e6a88de3be9a707107853605cbe63b65339893
huntinator7 <huntinator7@gmail.com>
2017-09-04 12:26:19 -0600

    Reverting discord picture

--- chat/index2.js
+++ chat/index2.js
@@ -53,7 +53,7 @@ client.on('ready', () => {
 
 //Any time a Discord message is sent, bot checks to see if in moosen-im channel and if not sent by bot. If so, it adds the message to the DB and emits it
 client.on('message', msg => {
-    // client.user.setAvatar('./images/discord.png');
+    client.user.setAvatar('./images/discord.png');
     if (msg.channel.id == 329020807487553537 && !(msg.author.bot)) {
         msg.channel.members.forEach(function (element) {
             try {
@@ -365,7 +365,7 @@ function sleep(ms) {
 async function sendToDiscord(un, msg, pic) {
     console.log('Taking a break...');
     client.guilds.get('176031369191882754').me.setNickname(un);
-    client.user.setAvatar(pic);
+    // client.user.setAvatar(pic);
     await sleep(1000);
     console.log('One second later');
     client.channels.get('329020807487553537').send(msg);
