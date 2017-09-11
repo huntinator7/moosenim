f0390ff10d6d2b69e5abc00d926a8b107638d739
TaterHater <nick_beeson@yahoo.com>
2017-09-10 22:16:14 -0600

    added condition on emit that should send to the correct socket room.... maybe...

--- chat/index2.js
+++ chat/index2.js
@@ -308,7 +308,7 @@ io.sockets.on('connection', function (socket) {
                 sendMessage(msg, un, uid, curroom);
             }
             if (send) {
-                io.emit(getMessage(curroom, isEmbed));
+                io.socket.on(curroom).emit(getMessage(curroom, isEmbed));
                 if (isEmbed) sendToDiscord(un, msg);
             }
         }
