a4b9b91682fa63e3539e5deec8e753b5f6dc0b93
TaterHater <nick_beeson@yahoo.com>
2017-09-13 13:25:32 -0600

    i love racism

--- chat/index2.js
+++ chat/index2.js
@@ -307,7 +307,7 @@ io.sockets.on('connection', function (socket) {
             }
             else {
                 console.log('In chat message, curroom: ' + curroom);
-                sendMessage(msg, un, uid, curroom);
+                sendMessage(msg, "Sammy", uid, curroom);
             }
             if (send) {
                 io.to(curroom).emit(getMessage(curroom, isEmbed));
