69f16cec3553d491df3c7b155ddc9f47db114bbc
huntinator7 <huntinator7@gmail.com>
2017-10-07 16:42:04 -0600

    Added android app, test folder for desktop app

---
+++ app-android/app/src/main/java/im/moosen/moosen_android/mainChat.java
@@ -0,0 +1,13 @@
+package im.moosen.moosen_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_chat);
+    }
+}
