88e4d8fbf8753e0019d99fd0341ec0f8c43be18c
huntinator7 <huntinator7@gmail.com>
2017-10-11 13:59:23 -0600

    Express middleware tut

--- app/index.js
+++ app/index.js
@@ -3,13 +3,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express')
+app.use((request, response, next) => {
+    console.log(request.headers)
+    next()
 })
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Error', err)
-    }
-    console.log(`Server is listening on ${port}`)
+app.use((request, response, next) => {
+    request.chance = Math.random()
+    next()
 })
+
+app.get('/', (request, response) => {
+    response.json({
+        chance: request.chance
+    })
+})
+
+app.listen(port)
