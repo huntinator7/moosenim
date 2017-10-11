c152376ba4024083fc4ef5c6342b512dfeef0d77
huntinator7 <huntinator7@gmail.com>
2017-10-11 13:56:48 -0600

    Setting up http server 2

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 const http = require('http')
+const express = require('express')
+const app = express()
 const port = 3000
 
-const requestHandler = (req, res) => {
-    console.log(req.url)
-    res.end('Hello Node.js server')
-}
+app.get('/', (req, res) => {
+    res.send('Hello from Express')
+})
 
-const server = http.createServer(requestHandler).listen(port, (err) => {
+app.listen(port, (err) => {
     if (err) {
-        return console.log('Error in listen', err)
+        return console.log('Error', err)
     }
-    console.log(`Server is listening on port ${port}`)
+    console.log(`Server is listening on ${port}`)
 })
