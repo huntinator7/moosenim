ae6c28db235a52eba1b6c1fa9fdecb439bd7bdc5
huntinator7 <harnell@mines.edu>
2017-11-24 20:48:10 -0700

    Changed auth to be in index

--- app/index.js
+++ app/index.js
@@ -3,6 +3,19 @@ const express = require('express')
 const passport = require('passport')
 const app = express()
 const port = 3000
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: '333736509560-id8si5cbuim26d3e67s4l7oscjfsakat.apps.googleusercontent.com',
+    clientSecret: ZCMQ511PhvMEQqozMGd5bmRH,
+    callbackURL: 'moosen.im:3000/login/google/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user)
+    })
+  }
+))
 
 app.use((request, response, next) => {
     console.log(request.headers)
@@ -25,7 +38,7 @@ app.get('/auth/google',
 )
 
 app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
+    passport.authenticate('google', { failureRedirect: '/auth/google' }),
     function (req, res) {
         res.post('Logged in successfully')
     }
