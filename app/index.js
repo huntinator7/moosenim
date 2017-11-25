8d67c97b9fc4bd565348d3539aa7e5bf0bdab451
Nick B <nick_beeson@yahoo.com>
2017-11-25 15:51:44 -0700

    Revert "Grammar"
    This reverts commit b15a21be7de188626a3ec7aa25e74757640be791.

--- app/index.js
+++ app/index.js
@@ -51,7 +51,7 @@ passport.serializeUser(function (user, cb) {
     cb(null, user)
 })
 
-passport.deserializeUser(function (obj, cb) {
+passport.deserializeUs  er(function (obj, cb) {
     cb(null, obj)
 })
 
