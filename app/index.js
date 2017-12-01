ee25facfe23c0016f13b2a1c9db2ef32b17f1d37
TaterHater <nick_beeson@yahoo.com>
2017-12-01 09:56:57 -0700

    added comments, testing if io is working.

--- app/index.js
+++ app/index.js
@@ -44,13 +44,13 @@ function onAuthorizeFail(data, message, error, accept) {
 //app.use(cookieParser)
 app.use(require('body-parser').urlencoded({ extended: true }))
 
-io.use(passportSocketIo.authorize({
-   // cookieParser: cookieParser,       // the same middleware you registrer in express
-    key: 'keyboard cat',       // the name of the cookie where express/connect stores its session_id
-    secret: 'keyboard cat',    // the session_secret to parse the cookie
-    store: sessionStore,        // we NEED to use a sessionstore. no memorystore please
-    fail: onAuthorizeFail,     // *optional* callback on fail/error - read more below
-}))
+//io.use(passportSocketIo.authorize({
+//    cookieParser: cookieParser,       // the same middleware you registrer in express
+//    key: 'keyboard cat',       // the name of the cookie where express/connect stores its session_id
+//    secret: 'keyboard cat',    // the session_secret to parse the cookie
+//    store: sessionStore,        // we NEED to use a sessionstore. no memorystore please
+//    fail: onAuthorizeFail,     // *optional* callback on fail/error - read more below
+//}))
 
 function onAuthorizeFail(data, message, error, accept) {
     if (error) throw new Error(message)
