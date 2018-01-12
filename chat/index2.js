f49a69bcb62db7626571a07881b2d01b526e9d9a
TaterHater <nick_beeson@yahoo.com>
2018-01-12 14:50:40 -0700

    fixing errors

--- chat/index2.js
+++ chat/index2.js
@@ -208,7 +208,7 @@ io.sockets.on('connection', function (socket) {
         var p = { uid: uid, x: x, y: y, color: 'red' };
         players.push(p);
 
-        console.log(uid);
+        console.log('vruid'+uid);
        
         socket.emit('vrUpdatePos',players);
     });
