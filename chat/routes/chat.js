3e9caa98c870ee33e00b086247c86a03440b0dae
TaterHater <nick_beeson@yahoo.com>
2018-01-21 00:16:59 -0700

    starting to get pissed

--- chat/routes/chat.js
+++ chat/routes/chat.js
@@ -3,6 +3,12 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.sendfile('chat/html/chat.html', {'root': '../'})
 })
+
+router.get('/login', function(req, res){
+   //res.sendfile('chat/html/login.html', {'root': '../'})
+   res.send("fuck routes"+__dirname)
+});
+
 router.post('/', function(req, res){
    res.send('POST route on chat.')
 });
