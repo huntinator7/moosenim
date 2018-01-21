0b28dbe4dd1b769029763cc05551082877cae9b2
TaterHater <nick_beeson@yahoo.com>
2018-01-20 18:08:51 -0700

    removed __dirname

--- chat/routes/chat.js
+++ chat/routes/chat.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res){
-   res.sendFile(__dirname + '../html/chat.html');
+   res.sendFile('../html/chat.html');
 });
 router.post('/', function(req, res){
    res.send('POST route on chat.');
