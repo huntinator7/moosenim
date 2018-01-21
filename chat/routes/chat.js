5fcd5762ba6e9a6c669c521a2d772960c391e962
TaterHater <nick_beeson@yahoo.com>
2018-01-20 18:13:05 -0700

    messing with routes

--- chat/routes/chat.js
+++ chat/routes/chat.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-router.get('/', function(req, res){
-   res.sendFile('../html/chat.html');
-});
+app.get('/', function(req, res) {
+  res.sendfile('/html/chat.html', {'root': '../'})
+})
 router.post('/', function(req, res){
-   res.send('POST route on chat.');
+   res.send('POST route on chat.')
 });
 
 //export this router to use in our index.js
