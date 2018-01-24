158cfe4f83df9c20f88d3acdab51426f9b72a650
huntinator7 <huntinator7@gmail.com>
2018-01-23 22:33:45 -0700

    Test for auth on /, probably won't work

--- chat/routes/routes.js
+++ chat/routes/routes.js
@@ -1,27 +1,31 @@
 var express = require('express');
 var router = express.Router();
-router.get('/', function(req, res) {
-  res.sendFile('/html/chat.html', {'root': '/var/www/html/chat'})
+router.get('/', function (req, res) {
+  passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.profile.emails.read', 'https://www.googleapis.com/auth/plus.login', 'profile', 'email'],
+    failureRedirect: '/login'
+  })
+  res.sendFile('/html/chat.html', { 'root': '/var/www/html/chat' })
 })
 
-router.get('/login', function(req, res) {
-  res.sendFile('/html/login.html', {'root': '/var/www/html/chat'})
+router.get('/login', function (req, res) {
+  res.sendFile('/html/login.html', { 'root': '/var/www/html/chat' })
 })
 
-router.get('/voice', function(req, res) {
-  res.sendFile('/html/voice.html', {'root': '/var/www/html/chat'})
+router.get('/voice', function (req, res) {
+  res.sendFile('/html/voice.html', { 'root': '/var/www/html/chat' })
 })
 
-router.get('/voicetest', function(req, res) {
-  res.sendFile('/html/voicetest.html', {'root': '/var/www/html/chat'})
+router.get('/voicetest', function (req, res) {
+  res.sendFile('/html/voicetest.html', { 'root': '/var/www/html/chat' })
 })
 
-router.get('/vr', function(req, res) {
-  res.sendFile('/html/vr.html', {'root': '/var/www/html/chat'})
+router.get('/vr', function (req, res) {
+  res.sendFile('/html/vr.html', { 'root': '/var/www/html/chat' })
 })
 
-router.post('/', function(req, res){
-   res.send('POST route on chat.')
+router.post('/', function (req, res) {
+  res.send('POST route on chat.')
 });
 
 //export this router to use in our index.js
