0197b4a9fd380ce34d2819a293f41ad95ab6352c
huntinator7 <huntinator7@gmail.com>
2018-01-25 02:09:04 -0700

    Fixed formatting

--- chat/routes/routes.js
+++ chat/routes/routes.js
@@ -2,27 +2,27 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res) {
-  if (req.user) {
-    res.sendFile('/html/chat.html', { 'root': '/var/www/html/chat' })
-  } else {
-    res.redirect('/login')
-  }
+    if (req.user) {
+        res.sendFile('/html/chat.html', { 'root': '/var/www/html/chat' })
+    } else {
+        res.redirect('/login')
+    }
 })
 
 router.get('/login', function (req, res) {
-  res.sendFile('/html/login.html', { 'root': '/var/www/html/chat' })
+    res.sendFile('/html/login.html', { 'root': '/var/www/html/chat' })
 })
 
 router.get('/voice', function (req, res) {
-  res.sendFile('/html/voice.html', { 'root': '/var/www/html/chat' })
+    res.sendFile('/html/voice.html', { 'root': '/var/www/html/chat' })
 })
 
 router.get('/voicetest', function (req, res) {
-  res.sendFile('/html/voicetest.html', { 'root': '/var/www/html/chat' })
+    res.sendFile('/html/voicetest.html', { 'root': '/var/www/html/chat' })
 })
 
 router.get('/vr', function (req, res) {
-  res.sendFile('/html/vr.html', { 'root': '/var/www/html/chat' })
+    res.sendFile('/html/vr.html', { 'root': '/var/www/html/chat' })
 })
 
 //export this router to use in our index.js
