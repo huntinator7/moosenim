ff9952c608f644e4ea683615f051f65ae69a2545
huntinator7 <huntinator7@gmail.com>
2018-02-04 14:44:56 -0700

    Removed unnecessary log message

--- chat/index.js
+++ chat/index.js
@@ -683,7 +683,7 @@ function getMessage(chatid, isEmbed, pic) {
     var nameString = "room" + chatid
     con.query("SELECT * FROM ( SELECT * FROM ?? ORDER BY id DESC LIMIT 1) sub ORDER BY  id ASC", [nameString], function (error, rows, results) {
 
-        console.log(rows)
+        // console.log(rows)
         if (error) throw error
         con.query("SELECT * FROM users WHERE users.name = ?", [rows[0].username], function (error, row) {
             if (pic) {
