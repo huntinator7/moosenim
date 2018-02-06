aed3846909c771e2e59ef4f20ea0b2b40d5883d9
huntinator7 <huntinator7@gmail.com>
2018-02-05 21:25:12 -0700

    I fix again

--- chat/index.js
+++ chat/index.js
@@ -205,9 +205,8 @@ var players = []
 //----SOCKET.IO----\\
 io.sockets.on('connection', function (socket) {
 
-
-
     console.log('CONNECTED to socket io: ' + socket.request.user.displayName)
+    getChatrooms(socket.id, socket.request.user.id)
     con.query("SELECT room_id FROM room_users WHERE user_id = ?", [socket.request.user.id], function (error, rows, results) {
         rows.forEach(function (element) {
             io.to(element).emit('login', socket.request.user.displayName, socket.request.user.emails[0].value, socket.request.user.photos[0].value, socket.request.user.id)
