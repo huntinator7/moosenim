a2cba8e881b0b326ec9afb3c96bbd3e130add4d3
huntinator7 <huntinator7@gmail.com>
2018-03-09 13:42:04 -0700

    Test list for commands 4.1

--- chat/js/chat.js
+++ chat/js/chat.js
@@ -223,7 +223,7 @@ $(function () {
 
     $('#cc-list').on('click', '.btn-ccrem', function() {
         console.log($(this).parent().parent().children('.btn-cc').text())
-        $('#cc-list').remove($(this).parent().parent())
+        $('#cc-list').remove(this)
     })
 
     socket.on('chat message', (user, msg, time, id, pic, room, badge) => {
