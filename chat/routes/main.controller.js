d064126505bd746bee1fd495d16673abd725b997
TaterHater <nick_beeson@yahoo.com>
2018-03-08 23:37:26 -0700

    really don't see this working

---
+++ chat/routes/main.controller.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var controller= require('../controllers/main.controller');
+var controllers = {
+
+
+
+
+}
+
+module.exports = controller;
