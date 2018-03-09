0841ede8b681d99bf71776188b4028cf83436750
TaterHater <nick_beeson@yahoo.com>
2018-03-08 23:26:46 -0700

    module fix

--- chat/controllers/main.controller.js
+++ chat/controllers/main.controller.js
@@ -1,6 +1,12 @@
 
-export function getChatrooms(sid, uid) {
+
+
+var MainController = {
+
+function getChatrooms(sid, uid) {
     con.query('SELECT * FROM rooms WHERE serialid IN (SELECT room_id FROM room_users WHERE user_id = ?)', [uid], (error, rows) => {
         io.to(sid).emit('roomlist', rows)
     })
+},
 }
+module.export = MainController
