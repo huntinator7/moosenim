04af86d85d5ca83602ebb76cf64264aebb6630be
TaterHater <nick_beeson@yahoo.com>
2018-03-08 23:49:13 -0700

    yah no

--- chat/controllers/main.controller.js
+++ chat/controllers/main.controller.js
@@ -5,7 +5,7 @@ class MainController  {
 constructor(){
     console.log('constucted')
 }
-  function getChatrooms(sid, uid) {
+   getChatrooms(sid, uid) {
     con.query('SELECT * FROM rooms WHERE serialid IN (SELECT room_id FROM room_users WHERE user_id = ?)', [uid], (error, rows) => {
         io.to(sid).emit('roomlist', rows)
     })
