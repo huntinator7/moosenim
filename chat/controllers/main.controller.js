be9d0b12452a59f16ced8590ca42f90b0898fe49
TaterHater <nick_beeson@yahoo.com>
2018-03-08 23:49:55 -0700

    kill me

--- chat/controllers/main.controller.js
+++ chat/controllers/main.controller.js
@@ -9,6 +9,6 @@ constructor(){
     con.query('SELECT * FROM rooms WHERE serialid IN (SELECT room_id FROM room_users WHERE user_id = ?)', [uid], (error, rows) => {
         io.to(sid).emit('roomlist', rows)
     })
-},
+}
 }
 module.exports = MainController
