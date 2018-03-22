47c9388e94aee9a1b65fc5b7ca2c2d5fb3ede307
TaterHater <nick_beeson@yahoo.com>
2018-03-22 14:19:18 -0600

    dont know if this'll help

--- chat/js/main.component.js
+++ chat/js/main.component.js
@@ -1,12 +1,7 @@
-var angular = require('angular');
-const ngRoute = require('angular-route')
-var routing = require( './main.routes')
+
 
 angular.module("myapp", [])
 
 .controller("HelloController", function($scope) {
    $scope.helloTo = {};
-   $scope.helloTo.title = "AngularJS-test";
-
-}
-);
+   $scope.helloTo.title = "AngularJS
