194075d38a43ad90f8313ad7229c8b42bb5d8100
TaterHater <nick_beeson@yahoo.com>
2018-03-21 23:27:24 -0600

    when all fails, hardcode.

--- chat/routes/business.js
+++ chat/routes/business.js
@@ -4,7 +4,7 @@ router.get('/', function(req, res){
     res.sendFile(__dirname + '../client/main/main.html');
 });
 router.get('/main', function(req, res){
-    res.sendFile(__dirname + '../client/main/main.html');
+    res.sendFile('/var/www/html/chat/client/main/main.html');
 });
 router.post('/', function(req, res){
    res.send('POST route on business.');
