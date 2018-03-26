67c78c0f6c9c1c165f70e25f478a78b604451271
TaterHater <nick_beeson@yahoo.com>
2018-03-26 00:36:44 -0600

    .

--- chat/index.js
+++ chat/index.js
@@ -383,7 +383,7 @@ io.sockets.on('connection', socket => {
     //Test emit
     socket.on('ping', name => {
         console.log('pong')
-        socket.emit('pong','test from server')
+        socket.emit('pong22','test from server')
         //console.log(Object.keys(io.sockets.sockets))
     })
 
