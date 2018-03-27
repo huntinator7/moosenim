d08a4f729eaa3d867a12598c719fc1016c7b0f80
TaterHater <nick_beeson@yahoo.com>
2018-03-26 21:19:00 -0600

    .

--- chat/index.js
+++ chat/index.js
@@ -385,7 +385,7 @@ socket.emit('pong2','test from server')
     socket.on('ping', name => {
         console.log('pong')
             con.query('SELECT * FROM ( SELECT * FROM room1 WHERE id < 100 ORDER BY id DESC LIMIT 5) sub ORDER BY id ASC', (error, rows, results) => {
-                socket.emit(rows)
+                socket.emit('pong2',rows)
             })
 
         //console.log(Object.keys(io.sockets.sockets))
