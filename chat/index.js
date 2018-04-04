ce5a0bcc6f8744ed92d3a62ddea57bd8cea2d422
TaterHater <nick_beeson@yahoo.com>
2018-04-04 11:10:27 -0600

    typo

--- chat/index.js
+++ chat/index.js
@@ -412,7 +412,7 @@ socket.emit('onconnect', socket.request.user.displayName)
         if (regex) addNewCommand(roomId, cmd, actn, msg, username, pic)
         else addNewCommand(roomId, escStrReg(cmd), actn, msg, username, pic)
     })
-    socket.on('get todo',)
+    
 
     socket.on('updateroomtheme', (params, icon, type, roomId) => {
         controller.changeRoomTheme(con, params, icon, type, roomId)
