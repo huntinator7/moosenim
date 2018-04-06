f15c1b465f1b5a2d2668d8f3bf84729e556eba53
TaterHater <nick_beeson@yahoo.com>
2018-04-05 23:51:33 -0600

    fixes

--- chat/index.js
+++ chat/index.js
@@ -412,7 +412,8 @@ socket.emit('onconnect', socket.request.user.displayName)
         if (regex) addNewCommand(roomId, cmd, actn, msg, username, pic)
         else addNewCommand(roomId, escStrReg(cmd), actn, msg, username, pic)
     })
-    socket.on('addtodo', (tags, msg, date, notes) => {
+    socket.on('addtodo', (tags, msg, date) => {
+        console.log('addtodo: ',msg,tags,date+note)
         addTODO (3, socket.request.user.id, tags, msg, date, notes)
     })
 
