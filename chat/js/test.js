858221c9d873e659e65b613ddb6f26701563a4e0
huntinator7 <huntinator7@gmail.com>
2018-04-10 02:59:00 -0600

    Adding test for RL 3

--- chat/js/test.js
+++ chat/js/test.js
@@ -1,4 +1,6 @@
-document.write('Testing1')
-console.log('Testing2')
-this.innerHTML = 'Testing3'
-document.getElementById('1').innerHTML = 'Testing4'
+(function () {
+    document.write('Testing1')
+    console.log('Testing2')
+    this.innerHTML = 'Testing3'
+    document.getElementById('1').innerHTML = 'Testing4'
+})
