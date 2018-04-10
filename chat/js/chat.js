8a43e21b10e0c1fe990a82af6c807da90922a51d
huntinator7 <huntinator7@gmail.com>
2018-04-09 19:38:48 -0600

    Actually changing value testing

--- chat/js/chat.js
+++ chat/js/chat.js
@@ -557,7 +557,7 @@ $(function () {
             state.timeId.forEach(e => {
                 console.log(e.id)
                 console.log(e.time)
-                console.log($('#' + e.id.toString()).children('.secondary-content'))
+                $('#' + e.id.toString()).children('.secondary-content').html(moment(e.time).fromNow())
             })
         }, 10000)
     }
