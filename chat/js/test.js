5258b6e60555a6d0582f454b0937f78bb49b9e29
huntinator7 <huntinator7@gmail.com>
2018-04-10 12:16:34 -0600

    Adding test for RL 5

--- chat/js/test.js
+++ chat/js/test.js
@@ -1,8 +1,12 @@
 (function test () {
-    document.write('Testing1')
-    console.log('Testing2')
-    this.innerHTML = 'Testing3'
-    document.getElementById('1').innerHTML = 'Testing4'
+    try {
+        document.write('Testing1')
+        console.log('Testing2')
+        this.innerHTML = 'Testing3'
+        document.getElementById('1').innerHTML = 'Testing4'
+    } catch (e) {
+        console.log('Oops')
+    }
 })()
 
 
