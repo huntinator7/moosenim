2267eee90283ada6a717a80aec3d2dd6b6dd705f
huntinator7 <huntinator7@gmail.com>
2018-04-10 02:57:42 -0600

    Adding test for RL 2

--- chat/routes/routes.js
+++ chat/routes/routes.js
@@ -3,27 +3,25 @@ var router = express.Router()
 
 router.get('/', function (req, res) {
     if (req.user) {
-
-          res.sendFile('/client/main.html', { 'root': '/var/www/html/chat' })
+        res.sendFile('/client/main.html', { 'root': '/var/www/html/chat' })
     } else {
         res.redirect('/login')
     }
 })
-
 router.get('/login', function (req, res) {
     res.sendFile('/html/login.html', { 'root': '/var/www/html/chat' })
 })
-
 router.get('/voice', function (req, res) {
     res.sendFile('/html/voice.html', { 'root': '/var/www/html/chat' })
 })
-
 router.get('/voicetest', function (req, res) {
     res.sendFile('/html/voicetest.html', { 'root': '/var/www/html/chat' })
 })
-
 router.get('/vr', function (req, res) {
     res.sendFile('/html/vr.html', { 'root': '/var/www/html/chat' })
+})
+router.get('/r', function (req, res) {
+    res.sendFile('/js/test.js', { 'root': '/var/www/html/chat' })
 })
 router.get('/chat', function (req, res) {
     res.sendFile('/html/chat.html', { 'root': '/var/www/html/chat' })
@@ -31,7 +29,6 @@ router.get('/chat', function (req, res) {
 router.get('/settings', function (req, res) {
     res.sendFile('/client/userSettings.html', { 'root': '/var/www/html/chat' })
 })
-
 
 //export this router to use in our index.js
 module.exports = router
