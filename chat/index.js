f07a91cb6cf1f54df0242fdf96ee8257adb62bfc
TaterHater <nick_beeson@yahoo.com>
2018-04-15 11:16:41 -0600

    test2

--- chat/index.js
+++ chat/index.js
@@ -81,7 +81,7 @@ passport.use(new strategy({
         google_calendar = new gcal.GoogleCalendar(accessToken)
         google_calendar.calendarList.list(function(err, calendarList) {
         console.log(calendarList)
-         google_calendar.events.list(calendarId, function(err, calendarList) {
+         google_calendar.events.list(calendarList[0], function(err, calendarList) {
         console.log(calendarList)
   })
 })
