26f8a3d0d31eb1a5730af072b6cf3c0181efeffd
TaterHater <nick_beeson@yahoo.com>
2018-04-16 12:17:31 -0600

    dumbass typo

--- chat/index.js
+++ chat/index.js
@@ -478,7 +478,7 @@ io.sockets.on('connection', socket => {
 			color: 'red'
 		}
 		players.push(p)
-        consolel.log('player info: '+p)
+        console.log('player info: '+p)
 		socket.emit('vrUpdatePos', players,socket.request.user.id)
 	})
 	setInterval(updateClient, 33)
