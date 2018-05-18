bd38c05cc6ad916ef2240143c31e59f0eb7324ab
huntinator7 <huntinator7@gmail.com>
2018-05-17 23:06:31 -0600

    multipart/form-data

--- chat/views/realEstate.jsx
+++ chat/views/realEstate.jsx
@@ -3,29 +3,11 @@ import React from 'react'
 class Main extends React.Component {
     constructor(props) {
         super(props)
-
-        this.state = {
-            imageURL: '',
-        }
-
-        this.handleUploadImage = this.handleUploadImage.bind(this)
-    }
-
-    handleUploadImage(ev) {
-        ev.preventDefault()
-
-        const data = new FormData()
-        data.append('files', this.uploadInput.files)
-
-        fetch('https://moosen.im/api/realestate', {
-            method: 'POST',
-            body: data,
-        })
     }
 
     render() {
         return (
-            <form onSubmit={this.handleUploadImage}>
+            <form action="/api/realestate" method="post" enctype="multipart/form-data">
                 <div>
                     <input ref={(ref) => { this.uploadInput = ref }} type="file" multiple />
                 </div>
