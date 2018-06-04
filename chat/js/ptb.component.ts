03817fbb438f92d9ee5d19a58217cd183bd45323
TaterHater <nick_beeson@yahoo.com>
2018-06-04 14:21:09 -0600

    testing angular

--- chat/js/ptb.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: '../chat/html/ptb.html',
-  //styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Periodic Table Of Business';
